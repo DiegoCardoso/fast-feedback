62f2d2d9fe2a1d37a8f605c548ae70caab753bc4
Diego Cardoso <diego@vaadin.com>
2021-01-06 18:02:38 +0200

    feat: add auth and enable GH signin (#1)

---
+++ utils/auth.js
@@ -0,0 +1,72 @@
+import { createContext, useContext, useEffect, useState } from "react"
+import firebase from './firebase'
+
+const authContext = createContext()
+
+export function AuthProvider({children}) {
+  const auth = useProvideAuth()
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>
+}
+
+export const useAuth = () => {
+  return useContext(authContext)
+}
+
+function useProvideAuth() {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const handleUser = rawUser => {
+    if (rawUser) {
+      const user = formatUser(rawUser)
+
+      setLoading(false)
+      setUser(user)
+      return user
+    } else {
+      setLoading(false)
+      setUser(false)
+      return false
+    }
+  }
+
+  const signinWithGithub = () => {
+    setLoading(true)
+    return firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GithubAuthProvider())
+      .then(response => handleUser(response.user))
+  }
+
+  const signout = () => {
+    return firebase
+      .auth()
+      .signOut()
+      .then(() => handleUser(false))
+  }
+
+  useEffect(() => {
+    const unsubsribe = firebase.auth().onAuthStateChanged(handleUser)
+
+    return () => unsubsribe()
+  }, [])
+
+  return {
+    user,
+    loading,
+    signinWithGithub,
+    signout
+  }
+}
+
+/**
+ * 
+ * @param {import("firebase").User} user 
+ */
+const formatUser = user => ({
+  uid: user.uid,
+  email: user.email,
+  name: user.displayName,
+  provider: user.providerData[0].providerId,
+  photoUrl: user.photoURL
+})
